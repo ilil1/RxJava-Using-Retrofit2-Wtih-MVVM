a98341307b404f144d7a2d37dfa0cbcbdd0e1354
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-08-03 23:39:44 +0900

    MainViewModel이 repository를 통해 데이터를 가져오도록 수정 MainViewModel에서 바로 RetroService를 이용해서 데이터를 가져오는 것이 아닌 repository를 이용하여 가져오도록 수정 BookRepository에서 단순히 RetroService를 이용하여 데이터를 가져오므로 따로 use case를 생성하진 않음

--- app/src/main/java/com/example/rxjavausingretrofit2withmvvm/di/AppCompositionRoot.kt
+++ app/src/main/java/com/example/rxjavausingretrofit2withmvvm/di/AppCompositionRoot.kt
@@ -1,6 +1,9 @@
 package com.example.rxjavausingretrofit2withmvvm.di
 
+import androidx.core.util.Supplier
 import com.example.rxjavausingretrofit2withmvvm.network.RetroService
+import com.example.rxjavausingretrofit2withmvvm.network.repository.BookRepositoryImpl
+import com.example.rxjavausingretrofit2withmvvm.viewModel.MainViewModel
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,7 +19,14 @@ class AppCompositionRoot {
             .build()
     }
 
-    val retroService by lazy {
+    private val retroService by lazy {
         retroInstance.create(RetroService::class.java)
     }
+
+    private val bookRepository get() = BookRepositoryImpl(retroService)
+    val viewModelSupplier get() = Supplier {
+        MainViewModel(bookRepository)
+    }
+
+    val viewModelFactory get() = MainViewModel.Factory(viewModelSupplier)
 }
--- app/src/main/java/com/example/rxjavausingretrofit2withmvvm/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/rxjavausingretrofit2withmvvm/viewModel/MainViewModel.kt
@@ -1,56 +1,59 @@
 package com.example.rxjavausingretrofit2withmvvm.viewModel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.util.Supplier
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.rxjavausingretrofit2withmvvm.network.BookListModel
-import com.example.rxjavausingretrofit2withmvvm.network.RetroInstance
-import com.example.rxjavausingretrofit2withmvvm.network.RetroService
+import com.example.rxjavausingretrofit2withmvvm.network.repository.BookRepository
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
-class MainViewModel : ViewModel() {
-
+class MainViewModel(
+    private val repository: BookRepository
+) : ViewModel() {
     private val _bookList: MutableLiveData<BookListModel> = MutableLiveData()
-    val bookList : LiveData<BookListModel> get() = _bookList
+    val bookList: LiveData<BookListModel> get() = _bookList
 
-
-    fun getBookListObserver() : LiveData<BookListModel>{
-        return bookList
-    }
-
-    fun makeApiCall(query: String) {
-
-        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
-        retroInstance.getBookListFromApi(query)
+    fun getBookList(query: String) {
+        repository.getBookList(query)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(getBookListObserverRx())
     }
 
-    private fun getBookListObserverRx() : Observer<BookListModel> {
-        return object : Observer<BookListModel>{
+    private fun getBookListObserverRx(): Observer<BookListModel> {
+        return object : Observer<BookListModel> {
             override fun onSubscribe(d: Disposable) {
-               //start showing progress indicator
+                //start showing progress indicator
             }
 
             override fun onNext(t: BookListModel) {
-                _bookList.postValue(t)
+                _bookList.value = t
             }
 
             override fun onError(e: Throwable) {
-                _bookList.postValue(null)
+                _bookList.value = null
             }
 
             override fun onComplete() {
-              //hide progress indicator
+                //hide progress indicator
             }
-
-
         }
     }
 
-
+    class Factory(
+        private val viewModelSupplier: Supplier<MainViewModel>
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return viewModelSupplier.get() as T
+        }
+    }
 }
