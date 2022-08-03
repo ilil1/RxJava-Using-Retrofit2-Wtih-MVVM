2399e34d82522b34d66e503b1e016d3638661051
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-08-04 00:00:16 +0900

    BookListAdapter code refactoring MyViewHolder를 nested class로 변경 Glide를 이용하여 image view를 설정하는 것을 method로 분리 bind method에서 nullable property에 대해 default 값 설정 및 imageLinks가 null이 아닐 경우에만 setImageView를 호출하도록 함

--- app/src/main/java/com/example/rxjavausingretrofit2withmvvm/adapter/BookListAdapter.kt
+++ app/src/main/java/com/example/rxjavausingretrofit2withmvvm/adapter/BookListAdapter.kt
@@ -2,30 +2,29 @@ package com.example.rxjavausingretrofit2withmvvm.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.rxjavausingretrofit2withmvvm.databinding.RecyclerListBinding
 import com.example.rxjavausingretrofit2withmvvm.network.VolumeInfo
 
 class BookListAdapter : RecyclerView.Adapter<BookListAdapter.MyViewHolder>() {
-
     var bookListData = ArrayList<VolumeInfo>()
 
-   inner class MyViewHolder(private val binding :RecyclerListBinding) : RecyclerView.ViewHolder(binding.root){
+    class MyViewHolder(private val binding: RecyclerListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        val title = binding.title
-        val description = binding.description
-        val publisher = binding.publisher
-        val imageView = binding.ImageView
+        fun bind(data: VolumeInfo) = with(binding) {
+            title.text = data.volumeInfo.title ?: "No title"
+            description.text = data.volumeInfo.description ?: "No description"
+            publisher.text = data.volumeInfo.publisher ?: "No publisher"
 
+            data.volumeInfo.imageLinks?.let {
+                setImageView(ImageView, it.smallThumbnail)
+            }
+        }
 
-        fun bind(data : VolumeInfo){
-            title.text = data.volumeInfo.title
-            description.text = data.volumeInfo.description
-            publisher.text = data.volumeInfo.publisher
-
-            val url = data .volumeInfo?.imageLinks?.smallThumbnail
-
+        private fun setImageView(imageView: ImageView, url: String) {
             Glide.with(imageView)
                 .load(url)
                 .circleCrop()
@@ -34,16 +33,17 @@ class BookListAdapter : RecyclerView.Adapter<BookListAdapter.MyViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding  = RecyclerListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding =
+            RecyclerListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       holder.bind(bookListData[position])
+        holder.bind(bookListData[position])
     }
 
     override fun getItemCount(): Int {
-       return  bookListData.size
+        return bookListData.size
     }
 }
