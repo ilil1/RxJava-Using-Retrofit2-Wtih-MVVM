2aa13a6fda1197b35606db1e64ea0965a7a03aa0
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-08-03 23:57:08 +0900

    BookInfo property 수정 BookInfo의 property들을 nullable하게 하여 NullPointerException 방지

--- app/src/main/java/com/example/rxjavausingretrofit2withmvvm/network/BookListModel.kt
+++ app/src/main/java/com/example/rxjavausingretrofit2withmvvm/network/BookListModel.kt
@@ -2,5 +2,5 @@ package com.example.rxjavausingretrofit2withmvvm.network
 
 data class BookListModel(val items : ArrayList<VolumeInfo>)
 data class VolumeInfo(val volumeInfo: BookInfo)
-data class BookInfo(val title:String, val publisher: String, val description : String, val imageLinks :ImageLinks)
+data class BookInfo(val title:String?, val publisher: String?, val description : String?, val imageLinks :ImageLinks?)
 data class ImageLinks(val smallThumbnail: String)
