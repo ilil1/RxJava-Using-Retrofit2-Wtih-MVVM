20d5634af6c73c81439f1a7546b57a136b73b1f5
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-08-03 23:54:59 +0900

    MainActivity 코드 refactoring Composition root을 통해 dependency들을 가져오도록 수정 viewModel과 bookListAdapter를 lazy하게 수정 initSearchBox 함수에서 onTextChanged에 viewModel을 이용하여 책 리스트를 가져오도록 함

--- app/src/main/java/com/example/rxjavausingretrofit2withmvvm/MainActivity.kt
+++ app/src/main/java/com/example/rxjavausingretrofit2withmvvm/MainActivity.kt
@@ -1,74 +1,74 @@
 package com.example.rxjavausingretrofit2withmvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
 import com.example.rxjavausingretrofit2withmvvm.adapter.BookListAdapter
 import com.example.rxjavausingretrofit2withmvvm.databinding.ActivityMainBinding
-import com.example.rxjavausingretrofit2withmvvm.network.BookListModel
 import com.example.rxjavausingretrofit2withmvvm.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val compositionRoot get() = (application as MyApplication).appCompositionRoot
 
-    private lateinit var binding : ActivityMainBinding
-    lateinit var viewModel : MainViewModel
-    lateinit var bookListAdapter : BookListAdapter
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel by lazy {
+        ViewModelProvider(this, compositionRoot.viewModelFactory)[MainViewModel::class.java]
+    }
+    private val bookListAdapter by lazy {
+        BookListAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        startObserving()
         initSearchBox()
         initRecyclerView()
     }
 
-    fun initSearchBox(){
-        binding.inputBookName.addTextChangedListener(object :TextWatcher{
+    private fun startObserving() {
+        viewModel.bookList.observe(this) {
+            if (it != null) {
+                // update adapter
+                bookListAdapter.bookListData = it.items
+                bookListAdapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initSearchBox() {
+        binding.inputBookName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                loadAPIData(s.toString())
+                viewModel.getBookList(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
 
             }
-
         })
     }
 
-
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         binding.recyclerView.apply {
+            adapter = bookListAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
-            val decoration  = DividerItemDecoration(applicationContext, VERTICAL)
-            addItemDecoration(decoration)
-            bookListAdapter = BookListAdapter()
-            adapter =bookListAdapter
+            addItemDecoration(
+                DividerItemDecoration(this@MainActivity, VERTICAL)
+            )
         }
-    }
-
-    fun loadAPIData(input: String){
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewModel.getBookListObserver().observe(this, Observer<BookListModel>{
-            if (it != null){
-                // update adapter
-                bookListAdapter.bookListData = it.items
-                bookListAdapter.notifyDataSetChanged()
-            } else{
-                Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
-            }
-        })
-        viewModel.makeApiCall(input)
     }
 }
